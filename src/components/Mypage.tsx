1747bd5892d018ac8888727ec8bc6ab55bc84f5f
ohayo-u <hskwhrk.ooaaaua.924@gmail.com>
2023-05-30 23:00:34 +0900

    add type definition to mypage.tsx

--- src/components/Mypage.tsx
+++ src/components/Mypage.tsx
@@ -1,44 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { User, onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "../firebase";
-import { onAuthStateChanged } from "firebase/auth";
 import { collection, onSnapshot, doc, getDoc } from "firebase/firestore";
+import { redirect } from "react-router-dom";
 import { Dish } from "./Dish";
 import { Food } from "./Food";
 import { Header } from "./Header";
 import { RegisterModal } from "./RegisterModal";
-import React, { useState, useEffect } from "react";
-import fixed_add_btn from "../images/fixed_add_btn.png";
 import { Login } from "./Login";
+import { FoodType } from "../models/food.model";
+import { DishType } from "../models/dish.model";
+import fixed_add_btn from "../images/fixed_add_btn.png";
 
 export const Mypage: React.FC = () => {
-  const [user, setUser] = useState<any>("");
-  const [loading, setLoading] = useState<any>(true);
-  const [isModalOpen, setIsModalOpen] = useState<any>(false);
-  const [dishList, setDishList] = useState<any>([]);
-  const [friendFoodList, setFriendFoodList] = useState<any>([]);
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [dishList, setDishList] = useState<DishType[]>([]);
+  const [friendFoodList, setFriendFoodList] = useState<FoodType[]>([]);
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       setLoading(false);
 
+      if (!currentUser) {
+        return redirect("/login");
+      }
+
       const dishListCollectionRef = collection(
         db,
         "users",
-        currentUser!.uid,
+        currentUser.uid,
         "dish-list"
       );
       const unsub = onSnapshot(dishListCollectionRef, (querySnapshot) => {
         setDishList(
-          querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+          querySnapshot.docs.map((doc) => ({
+            ...(doc.data() as {
+              name: string;
+              usedFoodId: string[];
+              imageURL?: string;
+            }),
+            id: doc.id,
+          }))
         );
 
-        const duplicatedFoodId: any = [];
+        const duplicatedFoodId: string[] = [];
         querySnapshot.docs.forEach((doc) => {
           duplicatedFoodId.push(...doc.data().usedFoodId);
         });
         const friendFoodIds = Array.from(new Set(duplicatedFoodId));
         Promise.all(
-          friendFoodIds.map(async (foodId: any) => {
+          friendFoodIds.map(async (foodId: string) => {
             const friendFoodDocRef = doc(db, "food", foodId);
             const documentSnapshot = await getDoc(friendFoodDocRef);
             return { id: foodId, name: documentSnapshot.data()!.name };
@@ -54,6 +68,7 @@ export const Mypage: React.FC = () => {
     <RegisterModal setIsModalOpen={setIsModalOpen} user={user} />
   ) : null;
 
+  console.log(dishList);
   return (
     <>
       {!loading && (
@@ -74,7 +89,7 @@ export const Mypage: React.FC = () => {
                         "repeat(auto-fill, minmax(100px, 1fr))",
                     }}
                   >
-                    {friendFoodList.map((friendFood: any) => (
+                    {friendFoodList.map((friendFood: FoodType) => (
                       <Food key={friendFood.id} food={friendFood} user={user} />
                     ))}
                   </div>
@@ -105,7 +120,7 @@ export const Mypage: React.FC = () => {
                         "repeat(auto-fill, minmax(240px, 1fr))",
                     }}
                   >
-                    {dishList.map((dish: any) => (
+                    {dishList.map((dish: DishType) => (
                       <Dish key={dish.id} dish={dish} user={user} />
                     ))}
                   </div>
