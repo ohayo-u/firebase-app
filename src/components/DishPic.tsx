4c4eb4e28172ccd9cff2f32938476a7552f86036
ohayo-u <hskwhrk.ooaaaua.924@gmail.com>
2023-05-31 12:21:50 +0900

    add type definition in DishPic.tsx

--- src/components/DishPic.tsx
+++ src/components/DishPic.tsx
@@ -1,32 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { ref, getDownloadURL } from "firebase/storage";
 import { storage } from "../firebase";
+import { dishInFM } from "../models/dishInFM.model";
 
 interface Props {
-  containDish: any;
+  dishInFM: dishInFM;
 }
 
 export const DishPic: React.FC<Props> = (props) => {
-  const [dishImgURL, setDishImgURL] = useState<any>();
+  const [dishImgURL, setDishImgURL] = useState<string>("");
 
   useEffect(() => {
     const imgPathRef = ref(
       storage,
-      props.containDish.imageURL !== undefined
-        ? props.containDish.imageURL
+      props.dishInFM.imageURL !== undefined
+        ? props.dishInFM.imageURL
         : "images/others/no_image.png"
     );
     getDownloadURL(imgPathRef).then((url) => {
       setDishImgURL(url);
     });
-  }, [props.containDish.imageURL]);
+  }, [props.dishInFM.imageURL]);
   return (
     <div>
       <img
         src={dishImgURL}
         style={{ width: "100%", aspectRatio: "16 / 11" }}
       ></img>
-      <p>{props.containDish.name}</p>
+      <p>{props.dishInFM.name}</p>
     </div>
   );
 };
