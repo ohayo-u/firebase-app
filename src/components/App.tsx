dc6751e071f82ce3a40802ac4d83dbf8e87067db
ohayo-u <hskwhrk.ooaaaua.924@gmail.com>
2023-06-14 20:21:43 +0900

    introducing ESLint and Prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,13 @@
-import "../App.scss";
-import { Routes, Route } from "react-router-dom";
-import { Login } from "./Login";
-import { Mypage } from "./Mypage";
+import '../App.scss';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './Login';
+import { Mypage } from './Mypage';
 
 export function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Mypage />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Mypage />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
